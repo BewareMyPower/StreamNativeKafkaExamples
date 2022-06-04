2feddbe8e298f5b9f03db8b6c893913f0567084b
Yunze Xu <xyzinfernity@163.com>
2022-06-04 18:40:38 +0800

    Add Confluent Avro Schema Example

---
+++ src/main/java/schema/ConfluentAvroSchemaExample.java
@@ -0,0 +1,41 @@
+package schema;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import schema.serializers.KafkaAvroConfigs;
+
+public class ConfluentAvroSchemaExample {
+
+    private static IndexedRecord createAvroRecord() {
+        String userSchema = "{\"namespace\": \"example.avro\", \"type\": \"record\", "
+                + "\"name\": \"User\", \"fields\": [{\"name\": \"name\", \"type\": \"string\"}]}";
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse(userSchema);
+        GenericRecord avroRecord = new GenericData.Record(schema);
+        avroRecord.put("name", "testUser");
+        return avroRecord;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(KafkaAvroConfigs.SCHEMA_REGISTRY_URL, "http://localhost:8001");
+
+        final KafkaProducer<Integer, Object> producer = new KafkaProducer<>(props);
+        final RecordMetadata metadata = producer.send(new ProducerRecord<>("xxx", createAvroRecord())).get();
+        System.out.println(metadata.offset());
+        producer.close();
+    }
+}
